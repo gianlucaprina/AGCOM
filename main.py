c70a92a15245cd264eadac53ac49acc9f6c9b0f2
Gianluca Prina <gianluca_prina@outlook.it>
2022-10-12 17:36:22 +0200

    Finding top 5 cities

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
+
 data = pd.read_csv('AGCOM_BBmap_opendata_comuni_csv.csv',encoding='utf-8',delimiter=';')
 
 datanew = data[['COMUNE','REGIONE','P1','SPEED_DOWN_ADSL_AVG']]
 
 datanew['AvgSpeedProCapite'] = datanew['SPEED_DOWN_ADSL_AVG'] / datanew['P1']
 print(datanew['AvgSpeedProCapite'])
+
+print(datanew.sort_values(by=['AvgSpeedProCapite'], ascending=False).head(5))
